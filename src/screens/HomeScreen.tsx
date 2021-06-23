8168ac711d20e1a43182a7dcf08c45dee4cc54d0
Andres Betancourt <aebb001@gmail.com>
2021-06-22 19:59:25 -0500

    haciendo el tipado para la peticion get axios

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,13 @@
 import React, {useEffect} from 'react';
 import {View, Text, Button, StatusBar} from 'react-native';
 import movieDB from '../api/movieDB';
+import {MovieDBNowPlaying} from '../interfaces/movieInterface';
 
 export const HomeScreen = ({navigation}: any) => {
   useEffect(() => {
-    movieDB.get('/now_playing').then(res => console.log(res));
+    movieDB
+      .get<MovieDBNowPlaying>('/now_playing')
+      .then(res => console.log(res.data));
   }, []);
 
   return (
